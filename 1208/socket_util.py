8282897105c14f30c2a15d150e23e5d131b4a996
kimyena1123 <kyq104@gmail.com>
2024-12-08 12:40:31 +0900

    feat: 이미 사용중인 포트를 다시 사용할 수 있도록 함

--- 1208/socket_util.py
+++ 1208/socket_util.py
@@ -1,7 +1,17 @@
 import socket
 
+"""
 def create_and_bind_socket(port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('', port))
+    server_socket.listen(5)
+    return server_socket
+"""
+
+def create_and_bind_socket(port):
+    """서버 소켓을 생성하고 바인딩"""
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # 포트 재사용 옵션 설정
     server_socket.bind(('', port))
     server_socket.listen(5)
     return server_socket
